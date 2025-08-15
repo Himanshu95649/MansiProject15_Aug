78aa7d56982cc82fa14f716ef6900fb2683863f9
Himanshu Arya <Himanshu Arya@Himanshu87>
2025-08-15 08:56:14 +0530

    Adding new project

---
+++ HimanshuPractice_Project/src/mansi/FirstClass.java
@@ -0,0 +1,11 @@
+package mansi;
+
+public class FirstClass {
+
+	public static void main(String[] args) {
+		System.out.println("Mansi");
+		System.out.println("Himanshu");
+
+	}
+
+}
---
+++ HimanshuPractice_Project/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module HimanshuPractice_Project {
+}
